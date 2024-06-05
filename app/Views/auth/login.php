b89da2aaf3849220a319f1520b33ca78d8838e2e
Okan Sajaaa <okannolgenol@gmail.com>
2024-06-05 18:53:44 +0700

    fix:logo login

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -25,7 +25,7 @@
 			<div class="w-1/2 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
 				<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
 					<a href="#" class="flex items-center mb-6 text-md font-semibold text-gray-900 dark:text-white">
-						<img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
+						<img class="w-8 h-8 mr-2" src="<?= base_url('logo.jpg') ?>" alt="logo">
 						CMS PESANTREN
 					</a>
 					<h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
