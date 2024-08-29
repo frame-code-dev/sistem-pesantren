0e19766bbb582f1d5da7b3b9b43c06cb76cb817c
Muhammad kholit <99956928+muhammadnorkholit@users.noreply.github.com>
2024-08-29 14:11:37 +0700

    fix(dinamis data): dashboard keuangan

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -6,6 +6,7 @@ namespace App\Controllers;
 use App\Models\Auth_model;
 use App\Models\BeritaModel;
 use App\Models\Santri_model;
+use App\Models\TransaksiModel;
 
 class Dashboard extends BaseController
 {
@@ -14,11 +15,21 @@ class Dashboard extends BaseController
 		$param['title'] = 'Dashboard';
 		$santri = new Santri_model;
 		$berita = new BeritaModel;
+		$transaksi = new TransaksiModel();
 		$param['total_santri'] = $santri->countData('aktif');
 		$param['total_alumni'] = $santri->countData('aktif');
 		$param['total_berita'] = $berita->countData('aktif');
 		$param['santri_aktif'] = $santri->getSantriAktifChart();
-		return view('dashboard',$param);
+		$param['santri_aktif'] = $santri->getSantriAktifChart();
+		$param['total_pendaftaran'] = $transaksi->getTotalPendaftaran();
+		$param['total_pendaftaran_ulang'] = $transaksi->getTotalPendaftaranUlang();
+		$param['total_pengeluaran'] = $transaksi->getTotalPengeluaran();
+		$param['chartPendaftaran'] = $transaksi->getTotalPengeluaran();
+		$param['getChartPendaftaran'] = $transaksi->getChartPendaftaran();
+		$param['getChartPemasukanPengeluaran'] = $transaksi->getChartPemasukanPengeluaran();
+		// return var_dump($param["getChartPendaftaran"]);
+		// exit();
+		return view('dashboard', $param);
 	}
 
 	// ... ada kode lain di sini ...
--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,4 +1,8 @@
-<?php $this->extend('template/app') ?>
+<?php
+
+use App\Helpers\Helpers;
+
+$this->extend('template/app') ?>
 
 <?= $this->section('content') ?>
 <div class="p-4 mt-14">
@@ -18,10 +22,10 @@
 				</div>
 				<div class="mt-3">
 					<h2 class="text-theme-text text-3xl font-bold tracking-tighter">
-						<?=$total_santri ?>
+						<?= $total_santri ?>
 					</h2>
 					<p class="text-gray-500 text-sm tracking-tighter">
-						Total Santri 
+						Total Santri
 					</p>
 				</div>
 			</div>
@@ -37,10 +41,10 @@
 				</div>
 				<div class="mt-3">
 					<h2 class="text-theme-text text-3xl font-bold tracking-tighter">
-						<?=$total_alumni ?>
+						<?= $total_alumni ?>
 					</h2>
 					<p class="text-gray-500 text-sm tracking-tighter">
-						Total Alumni 
+						Total Alumni
 					</p>
 				</div>
 			</div>
@@ -51,20 +55,20 @@
 					<div>
 						<button class="w-20 h-20 p-5 rounded-full bg-purple-100 flex align-middle items-center content-center mx-auto">
 							<svg class="text-3xl mt-1 text-purple-500 items-center content-center mx-auto" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7h1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V5a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h11.5M7 14h6m-6 3h6m0-10h.5m-.5 3h.5M7 7h3v3H7V7Z"/>
+								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7h1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V5a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h11.5M7 14h6m-6 3h6m0-10h.5m-.5 3h.5M7 7h3v3H7V7Z" />
 							</svg>
 						</button>
 					</div>
 					<div class="mt-3">
 						<h2 class="text-theme-text text-3xl font-bold tracking-tighter">
-							<?=$total_berita?>
+							<?= $total_berita ?>
 						</h2>
 						<p class="text-gray-500 text-sm tracking-tighter">
-							Total Berita 
+							Total Berita
 						</p>
 					</div>
 				</div>
-				
+
 			</div>
 		</div>
 	</div>
@@ -87,7 +91,7 @@
 			<div class="head flex lg:flex-row flex-col justify-between gap-5 mb-2">
 				<div class="title">
 					<h2 class="font-semibold tracking-tighter text-lg text-theme-text">
-						Persentase Santri 
+						Persentase Santri
 					</h2>
 				</div>
 			</div>
@@ -115,7 +119,7 @@
 					</div>
 					<div class="mt-3">
 						<h2 class="text-theme-text text-3xl font-bold tracking-tighter">
-
+							<?= Helpers::formatRupiah($total_pendaftaran->nominal) ?>
 						</h2>
 						<p class="text-gray-500 text-sm tracking-tighter">
 							Total Pendaftaran
@@ -137,6 +141,7 @@
 					</div>
 					<div class="mt-3">
 						<h2 class="text-theme-text text-3xl font-bold tracking-tighter">
+							<?= Helpers::formatRupiah($total_pendaftaran_ulang->nominal) ?>
 
 						</h2>
 						<p class="text-gray-500 text-sm tracking-tighter">
@@ -159,7 +164,7 @@
 					</div>
 					<div class="mt-3">
 						<h2 class="text-theme-text text-3xl font-bold tracking-tighter">
-
+							<?= Helpers::formatRupiah($total_pengeluaran->nominal) ?>
 						</h2>
 						<p class="text-gray-500 text-sm tracking-tighter">
 							Total Pengeluaran Pesantren
@@ -189,7 +194,7 @@
 			<div class="head flex lg:flex-row flex-col justify-between gap-5 mb-2">
 				<div class="title">
 					<h2 class="font-semibold tracking-tighter text-lg text-theme-text">
-						Persentase Pemasukan Dan Pengeluaran Pesantren 
+						Persentase Pemasukan Dan Pengeluaran Pesantren
 					</h2>
 				</div>
 			</div>
@@ -206,136 +211,135 @@
 <script>
 	// santri aktif-alumni 
 	var aktif_alumni = {
-          series: [{
-          name: 'Santri Aktif',
-          data:[
-			<?php foreach ($santri_aktif['data_aktif'] as $key => $value) { ?>
-				<?= $value ?>,
-			<?php } ?>
-		  ]
-        }, {
-          name: 'Alumni',
-          data: [
-			<?php foreach ($santri_aktif['data_alumni'] as $key => $value) { ?>
-				<?= $value ?>,
-			<?php } ?>
-		  ]
-        }],
-          chart: {
-          height: 350,
-          type: 'area'
-        },
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          curve: 'smooth'
-        },
-        xaxis: {
-          type: 'datetime',
-          categories: [
-			<?php foreach ($santri_aktif['categories'] as $key => $value) { ?>
-				"<?= $value ?>",
-			<?php } ?>
-		  ]
-        },
-        tooltip: {
-          x: {
-            format: 'dd/MM/yy HH:mm'
-          },
-        },
-        };
+		series: [{
+			name: 'Santri Aktif',
+			data: [
+				<?php foreach ($santri_aktif['data_aktif'] as $key => $value) { ?>
+					<?= $value ?>,
+				<?php } ?>
+			]
+		}, {
+			name: 'Alumni',
+			data: [
+				<?php foreach ($santri_aktif['data_alumni'] as $key => $value) { ?>
+					<?= $value ?>,
+				<?php } ?>
+			]
+		}],
+		chart: {
+			height: 350,
+			type: 'area'
+		},
+		dataLabels: {
+			enabled: false
+		},
+		stroke: {
+			curve: 'smooth'
+		},
+		xaxis: {
+			type: 'datetime',
+			categories: [
+				<?php foreach ($santri_aktif['categories'] as $key => $value) { ?> "<?= $value ?>",
+				<?php } ?>
+			]
+		},
+		tooltip: {
+			x: {
+				format: 'dd/MM/yy HH:mm'
+			},
+		},
+	};
 
 	var chart_aktif_alumni = new ApexCharts(document.querySelector("#aktif-alumni"), aktif_alumni);
 	chart_aktif_alumni.render();
-	
+
 	// aktif jenis kelamin
 	var aktif = {
-			series: [44, 55],
-			chart: {
+		series: [44, 55],
+		chart: {
 			width: 500,
 			type: 'pie',
-			},
-			labels: ['Laki-Laki', 'Perempuan'],
-			responsive: [{
+		},
+		labels: ['Laki-Laki', 'Perempuan'],
+		responsive: [{
 			breakpoint: 480,
 			options: {
 				chart: {
-				width: 500
+					width: 500
 				},
 				legend: {
-				position: 'bottom'
+					position: 'bottom'
 				}
 			}
-			}]
-    };
+		}]
+	};
 	var aktif = new ApexCharts(document.querySelector("#santri-gender"), aktif);
 	aktif.render();
-
 	// pendaftaran
 	var pendaftaran = {
-          series: [{
-          name: 'Pendaftaran',
-          data: [31, 40, 28, 51, 42, 109, 100]
-        }, {
-          name: 'Pendaftaran Ulang',
-          data: [11, 32, 45, 32, 34, 52, 41]
-        }],
-          chart: {
-          height: 350,
-          type: 'area'
-        },
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          curve: 'smooth'
-        },
-        xaxis: {
-          type: 'datetime',
-          categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-        },
-        tooltip: {
-          x: {
-            format: 'dd/MM/yy HH:mm'
-          },
-        },
-        };
+		series: [{
+			name: 'Pendaftaran',
+			data: <?= json_encode(array_values($getChartPendaftaran["pendaftaran"])) ?>
+		}, {
+			name: 'Pendaftaran Ulang',
+			data: <?= json_encode(array_values($getChartPendaftaran["pendaftaranUlang"])) ?>
+		}],
+		chart: {
+			height: 350,
+			type: 'area'
+		},
+		dataLabels: {
+			enabled: false
+		},
+		stroke: {
+			curve: 'smooth'
+		},
+		// xaxis: {
+		// 	type: 'datetime',
+		// 	categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+		// },
+		tooltip: {
+			x: {
+				format: 'dd/MM/yy HH:mm'
+			},
+		},
+	};
 
 	var chart_pendaftaran = new ApexCharts(document.querySelector("#pendaftaran"), pendaftaran);
 	chart_pendaftaran.render();
 	// pemasukan
 	var pemasukan = {
-        series: [{
-          name: 'Pemasukan',
-          data: [31, 40, 28, 51, 42, 109, 100]
-        }, 
-		{
-          name: 'Pengeluaran',
-          data: [11, 32, 45, 32, 34, 52, 41]
-        }],
-          chart: {
-          height: 350,
-          type: 'area',
-        },
-		colors: ["#2a9d8f","#FF0000"],
-        dataLabels: {
-          	enabled: false,
-		
-        },
-        stroke: {
-          curve: 'smooth'
-        },
-        xaxis: {
-          type: 'datetime',
-          categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-        },
-        tooltip: {
-          x: {
-            format: 'dd/MM/yy HH:mm'
-          },
-        },
-        };
+		series: [{
+				name: 'Pemasukan',
+				data: <?= json_encode(array_values($getChartPemasukanPengeluaran["pemasukan"])) ?>
+			},
+			{
+				name: 'Pengeluaran',
+				data: <?= json_encode(array_values($getChartPemasukanPengeluaran["pengeluaran"])) ?>
+			}
+		],
+		chart: {
+			height: 350,
+			type: 'area',
+		},
+		colors: ["#2a9d8f", "#FF0000"],
+		dataLabels: {
+			enabled: false,
+
+		},
+		stroke: {
+			curve: 'smooth'
+		},
+		// xaxis: {
+		// 	type: 'datetime',
+		// 	categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+		// },
+		tooltip: {
+			x: {
+				format: 'dd/MM/yy HH:mm'
+			},
+		},
+	};
 
 	var chart_pemasukan = new ApexCharts(document.querySelector("#pemasukan"), pemasukan);
 	chart_pemasukan.render();
